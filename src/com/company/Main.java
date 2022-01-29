a45eb713ed5f2f7b10d1a2b701c1702e28a7806c
Irina <online.info2579@gmail.com>
2022-01-29 11:05:55 -0500

    The Basic Structure is complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         SolarSystem milkyway=new SolarSystem();
+        milkyway.name=2;
+        milkyway.number=5;
     }
 }
